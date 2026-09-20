eae56aad5a29874d9f36f3cba2ecbde13e185676
noahpop77 <noahpop77@gmail.com>
2026-09-20 23:22:34 +0000

    update

--- main.c
+++ main.c
@@ -3,33 +3,49 @@
 #include <termios.h>
 
 struct termios enterCustomTermMode () {
+    // Enter alt mode
     printf("\x1b[?1049h\x1b[H");
     fflush(stdout);
-
+    
+    // Enter raw mode
     struct termios orig_termios, raw_termios;
     tcgetattr(STDIN_FILENO, &orig_termios);
-
+    
+    // Has to pass state of terminal to use in exitCustomTermMode
     return orig_termios;
 }
 
 void exitCustomTermMode (struct termios orig_termios) {
-    // 3. Clean up: Restore original termios attributes
+    // Restore original termios attributes
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 
-    // 4. Leave alternate screen buffer
+    // Leave alt screen buffer
     printf("\x1b[?1049l");
     fflush(stdout);
 }
 
+void printBox() {
+    for (int i = 0; i < 50 ; i ++) {
+        printf("\033[41m \033[0m");
+    }
+    printf("\n");
+}
+
 int main(void)
 {
-
     struct termios termSettings = enterCustomTermMode();
-    
+
     // ---- Your UI / Game Loop / Program goes here ----
     printf("STD_FILENO is an int with value: %d\n", STDIN_FILENO);
+    printf("STD_FILENO is an int with value: %d\n", STDOUT_FILENO);
+    printf("STD_FILENO is an int with value: %d\n", STDERR_FILENO);
+
+    printf("\033[31mTheWord\033[0m\n"); // Adds an alt code to text on the inside
+
+    printBox();
+
     printf("Welcome to Alt Mode!\n");
-    printf("Press any key to exit and restore your normal terminal...");
+    printf("Press enter to quit...");
     fflush(stdout);
 
     getchar(); 
---
+++ notes.md
@@ -0,0 +1,5 @@
+# Notes
+
+`gitupdate` - Push github updates in 1 go
+`./run` - helper script to run program
+
